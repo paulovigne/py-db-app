9e9b953feac28840a7b2a502ff0d2790dba20f3d
paulovigne <57726813+paulovigne@users.noreply.github.com>
2022-12-27 16:58:14 -0300

    Update pyapp.sql

--- sql/pyapp.sql
+++ sql/pyapp.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE pyapp;
 CREATE ROLE pyapp WITH LOGIN PASSWORD 'pyapppyapp';
 \c pyapp
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO pyapp;
 CREATE TABLE movies (id serial PRIMARY KEY,year integer,name text);
 INSERT INTO movies (year, name) VALUES (1994, 'The Shawshank Redemption'),(1972, 'The Godfather'),(2008, 'The Dark Knight');
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO pyapp;
